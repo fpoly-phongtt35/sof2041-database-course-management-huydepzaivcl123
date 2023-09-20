ec11b63b18ab3350f46eeec80b078fd4973db97e
huydepzaivcl123 <144806352+huydepzaivcl123@users.noreply.github.com>
2023-09-20 18:32:15 +0700

    Add files via upload

---
+++ Assignment.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE SOF2041_Assigment
+GO
+USE SOF2041_Assigment
+Go
+CREATE TABLE NhanVien(
+maNV NVARCHAR(50) primary key,
+matKhau NVARCHAR(50),
+hoTen NVARCHAR(50),
+vaiTro bit
+)
+CREATE TABLE ChuyenDe(
+maCD NVARCHAR(50) primary key,
+tenCD NVARCHAR(50),
+hocPhi float,
+thoiLuong int,
+hinh NVARCHAR(50),
+moTa NVARCHAR(100)
+)
+CREATE TABLE KhoaHoc(
+maKH INT IDENTITY(1,1) PRIMARY KEY,
+maCD NVARCHAR(50) not null,
+hocPhi float,
+thoiLuong int,
+ngayKhaiGiang date,
+ghiChu NVARCHAR(50),
+maNV NVARCHAR(50) NOT null,
+ngayTao Date,
+CONSTRAINT FK_KH_CD FOREIGN KEY(maCD) references ChuyenDe(maCD),
+CONSTRAINT FK_KH_NV FOREIGN KEY(maNV) references NhanVien(maNV)
+)
+CREATE TABLE NguoiHoc(
+maNH NVARCHAR(50) primary key,
+matKhau NVARCHAR(50),
+hoTen NVARCHAR(50),
+gioiTinh bit,
+ngaySinh date,
+email NVARCHAR(50),
+soDienThoai NVARCHAR(50),
+ghiChu NVARCHAR(50),
+maNV NVARCHAR(50) NOT null,
+CONSTRAINT FK_NH_NV FOREIGN KEY(maNV) references NhanVien(maNV),
+ngayDK date
+)
+CREATE TABLE HocVien(
+maHV INT IDENTITY(1,1) PRIMARY KEY,
+maKH INT  not null,
+maNH NVARCHAR(50) not null,
+CONSTRAINT FK_HV_KH FOREIGN KEY(maKH) references KhoaHoc(maKH),
+CONSTRAINT FK_HV_NH FOREIGN KEY(maNH) references NguoiHoc(maNH),
+diem float
+)
+
+
